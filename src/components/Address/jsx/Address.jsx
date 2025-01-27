4acb0acad3570dd281c0ed0544f690e1e6ecab53
천영성 <hokoro123@gmail.com>
2025-01-27 17:28:23 +0900

    address 개발 - 20% 완성 - 타이틀 달기 - 깃허브 로고 png 올리기 - 기존에 안쓰는 이미지 삭제

---
+++ src/components/Address/jsx/Address.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/address.css';
+
+const Address = () =>{
+    // Github 이미지 가져오기
+    const imageUrl = `${process.env.REACRT_APP_PUBLIC_URL}/images/Github.png`;
+
+
+
+    return(
+        <div className="Address">
+            <h1 className="Address-title">Address</h1>
+            <div className="Address-content">
+                <img src={imageUrl} alt="Logo"/>
+            </div>
+        </div>
+    );
+}
+
+export default Address;
--- src/components/Common/jsx/Layout.jsx
+++ src/components/Common/jsx/Layout.jsx
@@ -3,12 +3,14 @@ import Header from './Header';
 import Footer from './Footer';
 import Introduction from "../../Introduction/jsx/Introduction";
 import Skills from "../../Skills/jsx/Skills";
+import Address from "../../Address/jsx/Address";
 const Layout = ({children}) =>{
     return(
         <div className="Layout">
             <Header/>
             <Introduction/>
             <Skills/>
+            <Address/>
                 <main className="content">{children}</main>
             <Footer/>
         </div>
