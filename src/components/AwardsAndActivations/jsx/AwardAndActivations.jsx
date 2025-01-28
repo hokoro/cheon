8078e86df0adeffa59d6cfd2d438e9f66ec30cb5
천영성 <hokoro123@gmail.com>
2025-01-28 16:42:10 +0900

    디자인 수정

--- src/components/AwardsAndActivations/jsx/AwardAndActivations.jsx
+++ src/components/AwardsAndActivations/jsx/AwardAndActivations.jsx
@@ -14,7 +14,7 @@ const AwardAndActivations = () =>{
             <div className="Activation-content">
                 <p><b>청소년 교육지원 사업</b> (2020.05.04 ~ 2021.02.22)</p>
                 <p><b>ICT Innovation Square</b> (2020.10.19. ~ 2020.12.11)</p>
-                <p><b>광주 인공지능 사관학교</b> (2021.04.19. ~ 2021.12.07)</p>
+                <p><b>광주 인공지능사관학교</b> (2021.04.19. ~ 2021.12.07)</p>
                 <p><b>KT AIVLE School</b> (2023.08.08 ~ 2024.01.25)</p>
             </div>
         </div>
