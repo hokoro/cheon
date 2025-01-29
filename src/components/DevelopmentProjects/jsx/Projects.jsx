2ab1f61a148462b6375841a2acbabda1cf9a4392
천영성 <hokoro123@gmail.com>
2025-01-29 13:52:32 +0900

    Projects 컴포넌트 수정 - header 버튼 누를시 해당 항목으로 자동 스크롤

--- src/components/DevelopmentProjects/jsx/Projects.jsx
+++ src/components/DevelopmentProjects/jsx/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () =>{
 
     return(
 
-        <div className="Projects" name="Projects">
+        <div className="Projects" name="projects">
             <h1 className="Projects-title">Projects</h1>
             <div className="Projects-content">
                 <div className="Projects-content-piece">
